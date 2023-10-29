72414d3b089d2a801422e8166372334900d93ba2
Emersers <3065734564@qq.com>
2023-10-29 20:31:32 +0800

    modified:   navbar/zh.ts

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -6,7 +6,7 @@ export const zhNavbar = navbar([
     text:"主题相关",
     icon: "lightbulb",
     link: "https://theme-hope.vuejs.press/zh/",
-    prefix: "/themefile/",
+    prefix: "/theme/",
     children: [
       {
         text: "V2 文档",
