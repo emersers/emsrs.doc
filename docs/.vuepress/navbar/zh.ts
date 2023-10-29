eb9f5a4a1f4e35cf95743649275ec4e170a4e701
Emersers <3065734564@qq.com>
2023-10-29 23:30:45 +0800

    navbar changed

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -3,24 +3,25 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   {
-    text:"主题相关",
-    icon: "lightbulb",
-    link: "https://theme-hope.vuejs.press/zh/",
-    prefix: "/theme/",
-    children: [
-      {
-        text: "V2 文档",
-        icon: "book",
-        link: "https://theme-hope.vuejs.press/zh/",        
-      },
-      {
-        text: "功能与配置演示",
-        icon: "lightbulb",
-        link: "demo/",
-      },      
-    ],
+    text: "笔记",
+    icon: "book",
+    link: "/note/",
   },
-  
+  {
+    text: "Bot相关",
+    icon: "lightbulb",
+    link: "/bot/",
+  },
+  {
+    text: "网址导航",
+    icon: "lightbulb",
+    link: "/sites/",
+  },
+  {
+    text: "vercel",
+    icon: "laptop-code",
+    link: "https://vercel.com/emersers/emsrsdoc/",
+  },
   {
     text: "APP",
     // icon: "iconfont-select",
