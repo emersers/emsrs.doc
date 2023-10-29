b0a616af079016edd66ef7e07ca90ac2657013e7
Emersers <3065734564@qq.com>
2023-10-29 23:33:18 +0800

    sidebar changed

--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -3,24 +3,35 @@ import { sidebar } from "vuepress-theme-hope";
 export const zhSidebar = sidebar({
   "/": [
     "",
-    "sponsor",
+    {
+      text: "note",
+      icon: "book",
+      prefix: "note/",
+      // collapsible: true,
+      children: "structure",
+    },    
+  ],  
+  "/bot/":
+  [
+    "",
+  ],
+  "/theme/":
+  [
+    "",
     {
       text: "主题相关",
       icon: "laptop-code",
-      prefix: "theme/",
-      // link: "theme/README",
       collapsible: true,
-      // children: "structure",
-      children: [
-        "README",
-        {
-          text: "案例",
-          icon: "",
-          prefix: "demo/"
-        },
-        "slides",
-
-      ],
-    },    
+      children: "structure",
+    },
+  ],
+  "/codes/":
+  [
+    "",
+  ],
+  "/about/":
+  [
+    "",
+    "sponsor",
   ],
 });
