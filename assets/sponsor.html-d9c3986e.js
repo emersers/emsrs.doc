75ba4e259f31dc6db66d67734e2c39eb09b79541
emersers <61337388+emersers@users.noreply.github.com>
2023-11-12 01:17:59 +0000

    Deploying to gh-pages from @ emersers/emsrs.doc@7b6dd4a960267207d14cdd649ca299550ceacadb 🚀

---
+++ assets/sponsor.html-d9c3986e.js
@@ -0,0 +1,12 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as c,c as a,a as t,b as e,d as r,f as i}from"./app-b8285412.js";const d={},_=t("p",null,"如果你喜欢我的开源项目，并愿意给予支持的话，欢迎通过以下任意方式支持我：",-1),h={href:"https://github.com/emersers/emsrs.doc",target:"_blank",rel:"noopener noreferrer"},u=t("blockquote",null,[t("p",null,"留下一个小小的评论或者态度")],-1),p=t("blockquote",null,[t("p",null,"通过以下二维码 一次性捐款，打赏作者一杯茶。🍵")],-1),m=t("p",null,"谢谢！ ❤️",-1),g=t("thead",null,[t("tr",null,[t("th",{style:{"text-align":"center"}},"微信赞赏"),t("th",{style:{"text-align":"center"}},"微信"),t("th",{style:{"text-align":"center"}},"支付宝")])],-1),f={style:{"text-align":"center"}},b=["src"],x={style:{"text-align":"center"}},y=["src"],B={style:{"text-align":"center"}},k=["src"],q=t("h2",{id:"公众号",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#公众号","aria-hidden":"true"},"#"),e(" 公众号")],-1),w=t("ul",null,[t("li",null,"咕咕咕~")],-1),E=t("p",null,"还有更多好玩的等你探索~",-1),N=t("h2",{id:"致谢",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#致谢","aria-hidden":"true"},"#"),e(" 致谢")],-1),V=t("p",null,"感谢给予支持的朋友，您的支持是我前进的动力 🎉",-1);function v(n,L){const s=l("ExternalLinkIcon");return c(),a("div",null,[_,t("blockquote",null,[t("p",null,[e("点个Star或者分享 "),t("a",h,[e("emsrs.doc🚀"),r(s)])])]),u,p,m,t("table",null,[g,t("tbody",null,[t("tr",null,[t("td",f,[t("img",{src:n.$withBase("/assets/qrcode/Wechat_500.png"),alt:"赞赏码",width:"180"},null,8,b)]),t("td",x,[t("img",{src:n.$withBase("/assets/qrcode/Wechat_sponsor_500.png"),alt:"Wechat QRcode",width:"180"},null,8,y)]),t("td",B,[t("img",{src:n.$withBase("/assets/qrcode/alipay_500.png"),alt:"Alipay QRcode",width:"180"},null,8,k)])])])]),i(` ## 成为赞助商
+
+成为赞助商，即可将您的品牌LOGO及标语同时展示在：
+
+- GitHub仓库主页顶部
+
+- 官网首页顶部
+
+- 官网所有文档页左侧边栏顶部
+
+
+[联系方式](https://xugaoyi.com/about/#%E8%81%94%E7%B3%BB) `),q,w,E,N,V])}const C=o(d,[["render",v],["__file","sponsor.html.vue"]]);export{C as default};
